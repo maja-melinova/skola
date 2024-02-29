ebb3c1be1aa4467a676ed66d349d44fb2b2fbbeb
Marie Melínová <maja.melinova@gmail.com>
2024-02-29 13:14:56 +0000

    .

--- LS2024/Vicerozmerna statistika/VS_cviko.R
+++ LS2024/Vicerozmerna statistika/VS_cviko.R
@@ -3,5 +3,5 @@ library(haven)
 data3_1 = read_sav("data/data3_1.sav")
 
 summary(data3_1[,-c(1:5)])
-qqnorm(data3_1$KRK, pch = 1, frame = FALSE)
-qqline(data3_1$KRK, col = "steelblue", lwd = 2)
+qqnorm(data3_1$HRUDNIK, pch = 1, frame = FALSE)
+qqline(data3_1$HRUDNIK, col = "steelblue", lwd = 2)
